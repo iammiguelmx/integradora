ca80dc374dc2de3c64c240f6096d8029feba295d
Miguel <miguelcamacho@gmail.com>
2019-12-03 12:34:07 -0600

    mensaje detallado del commit

--- static/js/fb.js
+++ static/js/fb.js
@@ -23,7 +23,7 @@ window.fbAsyncInit = function () {
         appId: '2486509434954225',
         cookie: true,                     // Enable cookies to allow the server to access the session.
         xfbml: true,                     // Parse social plugins on this webpage.
-        version: 'v3.3'                 // Use this Graph API version for this call.
+        version: 'v5.0'                 // Use this Graph API version for this call.
     });
 
 
