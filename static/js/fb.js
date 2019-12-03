c549f6fd8995d720bbe2e65e46413f2cd245106c
Miguel <miguelcamacho@gmail.com>
2019-12-03 11:40:47 -0600

    Funtion js

---
+++ static/js/fb.js
@@ -0,0 +1,31 @@
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '2486509434954225',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+    FB.AppEvents.logPageView();   
+  };
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/es_LA/sdk.js#xfbml=1&version=v2.9&appId=2486509434954225";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+  //3. Check Login Status
+FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+});
+
+//4.Esta es la devolución de llamada. Llama a FB.getLoginStatus()
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+
+
