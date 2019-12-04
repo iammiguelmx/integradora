8b85af6aab88bc23a22f3c7fc745db4498fdd2f1
Miguel Camacho <miguelangelcamachocampos@gmail.com>
2019-12-03 21:21:48 -0600

    Rutes :alien:

---
+++ static/js/alertas.js
@@ -0,0 +1,12 @@
+alertify.confirm("This is a confirm dialog.",
+  function(){
+    alertify.success('Ok');
+  },
+  function(){
+    alertify.error('Cancel');
+  });
+
+
+function agregarCorrecto() {
+    console.log("Hello world!");
+}
